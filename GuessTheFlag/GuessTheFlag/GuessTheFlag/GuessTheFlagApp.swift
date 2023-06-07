fab9a4682238c6ba0efe3c3d0771cb5835941933
Jenn <j3nnkkin@gmail.com>
2023-06-07 17:50:51 +0900

    [GuessTheFlag] Initial commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jenn Park on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
