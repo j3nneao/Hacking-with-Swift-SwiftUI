ad4dac8db89e94ae4e6d688e05942502422cc938
Jenn <j3nnkkin@gmail.com>
2023-06-19 21:45:57 +0900

    BetterRest Project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jenn Park on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
