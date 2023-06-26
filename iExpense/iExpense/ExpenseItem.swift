223b4b1ed590c10bf8dd47d7749608d3d80f2e2c
Jenn <j3nnkkin@gmail.com>
2023-06-26 17:05:30 +0900

    iExpense project

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,35 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Jenn Park on 2023-06-26.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet { // didSet property observer - when the items property is updated, execute the code in didSet (called AFTER the setter is called)
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jenn Park on 2023-06-23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
