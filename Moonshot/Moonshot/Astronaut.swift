20a53f6e3b8786c8a5b0c94cfe3a0522f5484971
Jenn <j3nnkkin@gmail.com>
2023-06-28 20:22:34 +0900

    [Moonshot] decode and display JSON

--- Moonshot/Moonshot/Astronaut.swift
+++ Moonshot/Moonshot/Astronaut.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct Astronaut: Codable, Identifiable { // Codable so we can construct instances straight from JSON, Identifiable so we can use in ForEach
+    let id: String
+    let name: String
+    let description: String
+}
