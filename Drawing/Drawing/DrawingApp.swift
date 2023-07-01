bdda4d33845247438a9e70a8883b9be4cf5ec0bb
Jenn <j3nnkkin@gmail.com>
2023-07-01 20:54:30 +0900

    [Drawing] Initial commit

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jenn Park on 2023-07-01.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
