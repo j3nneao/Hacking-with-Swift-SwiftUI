131e33038687be8ce4558d71364336f45a41495c
Jenn <j3nnkkin@gmail.com>
2023-07-05 21:35:20 +0900

    [CupcakeCorner] project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jenn Park on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Jenn Park on 2023-07-05.
+//
+
+import Foundation
