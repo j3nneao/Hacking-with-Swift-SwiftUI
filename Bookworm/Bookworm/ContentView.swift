67ac43bd32006a81a9bf680e9727c5df55747e17
Jenn <j3nnkkin@gmail.com>
2023-07-07 20:09:19 +0900

    [Bookworm] project

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Jenn Park on 2023-07-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [ SortDescriptor(\.title) ]) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add book", systemImage: "plus")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
+        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+            try? moc.save()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    struct WrappedView: View {
+        @StateObject private var dataController = DataController()
+
+        var body: some View {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+    
+    static var previews: some View {
+        WrappedView()
+    }
+}
---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Jenn Park on 2023-07-07.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
