c73b3e7e8379c4298b77f33dbd18298446810c3d
Jenn <j3nnkkin@gmail.com>
2023-07-10 22:12:33 +0900

    [CoreDataProject] Initial commit

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Jenn Park on 2023-07-10.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
